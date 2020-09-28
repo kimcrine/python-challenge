bec6117b9bf7752af6953a7ce42926c7a2f8c3e1
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-09-28 17:40:50 -0400

    create lists to store data and initialize variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,17 @@ import csv
 #Establish path for CSV file
 pybank_csv = os.path.join(".", "Resources", "budget_data.csv")
 
+#Create lists to store data
+months = []
+profit_loss_changes = []
+
+#Initialize variables
+count = 0
+net_profit_loss = 0
+initial_profit_loss = 0
+current_profit_loss = 0
+delta_profit_loss = 0
+
 
 
         

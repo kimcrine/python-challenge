4c37896b7ea4d2f60d7d6b976e982c00ce126ade
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-09-28 17:40:05 -0400

    import modules and set path for CSV file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,18 +3,8 @@ import os
 import csv
 
 #Establish path for CSV file
-pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
+pybank_csv = os.path.join(".", "Resources", "budget_data.csv")
 
-#Create lists to store data
-profit = []
-monthly_changes = []
-date = []
 
-#Initialize variables
-count = 0
-total_profit = 0
-total_change_profits = 0
-initial_profit = 0
 
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+        

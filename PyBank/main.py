480efcf1496e57d93b24255886a0b8bd0b16f3a2
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-09-28 16:37:15 -0400

    created lists to store data and initialized variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,20 @@
+#Import modules
 import os
 import csv
 
-pybank_csv = os.path.join("Desktop", "Bootcamp Work", "Homework 3", "python-challenge", "PyBank", "Resources", "budget_data.csv")
+#Establish path for CSV file
+pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+#Create lists to store data
+profit = []
+monthly_changes = []
+date = []
+
+#Initialize variables
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
 
 with open(pybank_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")

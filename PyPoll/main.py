b77b15bb974aa3a11adadef19cfd78de722b4bd1
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-09-29 15:01:11 -0400

    initiated for loop and candidate counter

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,8 +8,16 @@ pypoll_csv = os.path.join(".", "Resources", "election_data.csv")
 #Create lists to store data
 candidates = []
 votes_per_candidate = []
+name = []
 
 #Open and read CSV
 with open(pypoll_csv, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file)
+
+     #Read through each row of data
+     for row in csv_reader:
+         candidates.append(row[2])
+
+    #Count votes per candidate and add to the list
+    candidate_count = [[x,candidates.count(x)] for x in set (candidates)]
